234b8b6fe51f9d463a6be3c85fc7363f69bf7672
VictoriaWisdom <162807284+VictoriaWisdom@users.noreply.github.com>
2024-11-24 20:57:12 +0300

    New Structers

---
+++ ClassesProject/Interval.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProject
+{
+    internal struct Interval
+    {
+        public Interval(int minValue, int maxValue) { Min = minValue; Max = maxValue; }
+
+        public int Get { get { return _random.Next(Min, Max); } }
+        public int Min { get; }
+        public int Max { get; }
+        public int Average { get { return (Max + Min) / 2; } }
+
+
+
+        private Random _random = new Random();
+
+        
+    }
+}
---
+++ ClassesProject/Rate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesProject
+{
+    internal struct Rate
+    {
+        public Unit Unit;
+        public int Damege;
+        public float Health;
+
+        public Rate() { }
+    }
+}
